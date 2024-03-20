087bf2ed1e3fd87b4a3f49d8d9c02b5ed26ba8e7
hossein javadi <hosseinj13@yahoo.com>
2024-03-20 07:22:58 +0330

    Fixed PersonService.java

--- src/main/java/two/service/PersonService.java
+++ src/main/java/two/service/PersonService.java
@@ -3,6 +3,8 @@ package two.service;
 import two.PersonRepository;
 import two.model.Person;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
 
 
 public class PersonService {
@@ -12,10 +14,11 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public Person signUp(String firstName, String lastName) {
+    public Person signUp(String firstName, String lastName, Date dateOfBirth) {
         Person person = new Person();
         person.setFirstName(firstName);
         person.setLastName(lastName);
+        person.setDateOfBirth(dateOfBirth);
         return personRepository.save(person);
     }
 }
