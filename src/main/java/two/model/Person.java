28f099ed4494490552da492c21e97cccea8ae7fc
hossein javadi <hosseinj13@yahoo.com>
2024-03-21 00:33:33 +0330

    Fixed Person.java Using Validation Annotations.

--- src/main/java/two/model/Person.java
+++ src/main/java/two/model/Person.java
@@ -1,6 +1,10 @@
 package two.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import java.util.Date;
@@ -18,8 +22,17 @@ import java.util.Date;
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private Date dateOfBirth;
+    Long id;
+
+    @NotBlank(message = "First name is required and should contain only letters")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "firstName must contain only letters")
+    String firstName;
+
+    @NotBlank(message = " lastName is required")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "lastName must contain only letters")
+    String lastName;
+
+    @NotNull(message = "Birth date is required")
+    @Past(message = "Birth date must be in the past")
+    Date dateOfBirth;
 }
