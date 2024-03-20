21816f9377574a73f359463a65e2aa9203cba284
hossein javadi <hosseinj13@yahoo.com>
2024-03-21 00:40:25 +0330

    Fixed TeacherService.java Using  Hibernate Validation.

--- src/main/java/two/service/TeacherService.java
+++ src/main/java/two/service/TeacherService.java
@@ -1,11 +1,16 @@
 package two.service;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import two.model.Teacher;
 import two.model.TeacherRank;
 
 import java.util.Date;
+import java.util.Set;
 
 public class TeacherService {
     private final Session session;
@@ -22,6 +27,20 @@ public class TeacherService {
         teacher.setTeacherCode(teacherCode);
         teacher.setTeacherRank(teacherRank);
         teacher.setMonthlySalary(monthlySalary);
+
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Teacher>> violations = validator.validate(teacher);
+
+        if (!violations.isEmpty()) {
+            for (ConstraintViolation<Teacher> violation : violations) {
+                System.out.println(violation.getMessage());
+            }
+            System.out.println("Sign up failed due to validation errors.");
+            return null;
+        }
+
 
         Transaction transaction = session.beginTransaction();
         session.save(teacher);
