19d53b0f6cefe9d7b1bd9463e8493cf8f0f40dc8
hossein javadi <hosseinj13@yahoo.com>
2024-03-21 00:34:20 +0330

    Fixed Student.java Using Validation Annotations.

--- src/main/java/two/model/Student.java
+++ src/main/java/two/model/Student.java
@@ -2,8 +2,12 @@ package two.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
 
 @Entity
 @DiscriminatorValue("student")
@@ -14,10 +18,19 @@ import lombok.experimental.FieldDefaults;
 @Getter
 @Setter
 
-public class Student extends Person{
+public class Student extends Person {
 
-     String studentCode;
-     String fieldOfStudy;
-     int entranceYear;
+    @NotBlank(message = "Student code is required")
+    @Size(min = 6, max = 10, message = "Student code must be between 6 and 10 characters")
+    String studentCode;
+
+    @NotBlank(message = "Field of study is required")
+    String fieldOfStudy;
+
+    @NotNull(message = "Entrance year is required")
+    @Positive(message = "Entrance year must be positive")
+    @Min(value = 1900, message = "Entrance year must be greater than or equal to 1900")
+    int entranceYear;
+
 
 }
